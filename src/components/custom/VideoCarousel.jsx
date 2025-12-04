e41fa0b207632e8e4433a78a147837eb28a9a362
Muhammad Aaqib <162022683+aqibaa@users.noreply.github.com>
2025-12-04 15:30:08 +0500

    Change poster image extensions to .png
    Updated poster image file extensions from .jpg to .png for all video entries.

--- src/components/custom/VideoCarousel.jsx
+++ src/components/custom/VideoCarousel.jsx
@@ -9,22 +9,22 @@ import {
 
 
 const videos = [
-  { id: 3, videoSrc: "/videos/v3.mp4", posterSrc: "/posters/poster3.jpg" },
-  { id: 2, videoSrc: "/videos/v2.mp4", posterSrc: "/posters/poster2.jpg" },
-  { id: 5, videoSrc: "/videos/v5.mp4", posterSrc: "/posters/poster5.jpg" },
-  { id: 11, videoSrc: "/videos/v11.mp4", posterSrc: "/posters/poster1.jpg" },
-  { id: 6, videoSrc: "/videos/v6.mp4", posterSrc: "/posters/poster6.jpg" },
-  { id: 9, videoSrc: "/videos/v9.mp4", posterSrc: "/posters/poster9.jpg" },
-  { id: 4, videoSrc: "/videos/v4.mp4", posterSrc: "/posters/poster4.jpg" },
-  { id: 8, videoSrc: "/videos/v8.mp4", posterSrc: "/posters/poster8.jpg" },
-  { id: 10, videoSrc: "/videos/v10.mp4", posterSrc: "/posters/poster10.jpg" },
-  { id: 12, videoSrc: "/videos/v12.mp4", posterSrc: "/posters/poster12.jpg" },
-  { id: 14, videoSrc: "/videos/v14.mp4", posterSrc: "/posters/poster14.jpg" },
-  { id: 19, videoSrc: "/videos/v19.mp4", posterSrc: "/posters/poster19.jpg" },
-  { id: 13, videoSrc: "/videos/v13.mp4", posterSrc: "/posters/poster13.jpg" },
-  { id: 15, videoSrc: "/videos/v15.mp4", posterSrc: "/posters/poster15.jpg" },
-  { id: 20, videoSrc: "/videos/v20.mp4", posterSrc: "/posters/poster20.jpg" },
-  { id: 17, videoSrc: "/videos/v17.mp4", posterSrc: "/posters/poster17.jpg" },
+  { id: 3, videoSrc: "/videos/v3.mp4", posterSrc: "/posters/poster3.png" },
+  { id: 2, videoSrc: "/videos/v2.mp4", posterSrc: "/posters/poster2.png" },
+  { id: 5, videoSrc: "/videos/v5.mp4", posterSrc: "/posters/poster5.png" },
+  { id: 11, videoSrc: "/videos/v11.mp4", posterSrc: "/posters/poster11.png" },
+  { id: 6, videoSrc: "/videos/v6.mp4", posterSrc: "/posters/poster6.png" },
+  { id: 9, videoSrc: "/videos/v9.mp4", posterSrc: "/posters/poster9.png" },
+  { id: 4, videoSrc: "/videos/v4.mp4", posterSrc: "/posters/poster4.png" },
+  { id: 8, videoSrc: "/videos/v8.mp4", posterSrc: "/posters/poster8.png" },
+  { id: 10, videoSrc: "/videos/v10.mp4", posterSrc: "/posters/poster10.png" },
+  { id: 12, videoSrc: "/videos/v12.mp4", posterSrc: "/posters/poster12.png" },
+  { id: 14, videoSrc: "/videos/v14.mp4", posterSrc: "/posters/poster14.png" },
+  { id: 19, videoSrc: "/videos/v19.mp4", posterSrc: "/posters/poster19.png" },
+  { id: 13, videoSrc: "/videos/v13.mp4", posterSrc: "/posters/poster13.png" },
+  { id: 15, videoSrc: "/videos/v15.mp4", posterSrc: "/posters/poster15.png" },
+  { id: 20, videoSrc: "/videos/v20.mp4", posterSrc: "/posters/poster20.png" },
+  { id: 17, videoSrc: "/videos/v17.mp4", posterSrc: "/posters/poster17.png" },
 ];
 
 const LazyVideo = ({ videoSrc, posterSrc }) => {
